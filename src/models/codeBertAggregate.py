e440c576eb0740e840e3b652ef678e857d1645dc
MarcoTomasone <tomasone35@gmail.com>
2024-05-24 16:19:21 +0200

    add: codebert agg evaluation

--- src/models/codeBertAggregate.py
+++ src/models/codeBertAggregate.py
@@ -10,29 +10,35 @@ from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampl
 from transformers import AutoModel, RobertaTokenizer
 from torch import cuda, nn
 from torch.nn.utils.rnn import pad_sequence
+from tqdm import tqdm
 
+# Set up logging
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+print("Start!")
+logger.info("Starting script...")
 
 #region constants
 MAX_LEN = 512
-CODE_BLOCKS = 4
+CODE_BLOCKS = 2
 SAFE_IDX = 4
-TRAIN_BATCH_SIZE = 8
+TRAIN_BATCH_SIZE = 4
 VALID_BATCH_SIZE = 16
 EPOCHS = 20
 LEARNING_RATE = 1e-05
 NUM_CLASSES = 5
-MODEL = 'codeBertAgg'
+MODEL = 'codeBertAggMean'
 TOKENIZER = RobertaTokenizer.from_pretrained('microsoft/codebert-base')
 DEVICE = 'cuda' if cuda.is_available() else 'cpu'
 if DEVICE == 'cuda':
-    print(torch.cuda.get_device_name(0))
-    print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+    logger.info(f"Using GPU: {torch.cuda.get_device_name(0)}")
+    logger.info(f"Memory Usage: Allocated: {round(torch.cuda.memory_allocated(0)/1024**3,1)} GB, Cached: {round(torch.cuda.memory_reserved(0)/1024**3,1)} GB")
+else:
+    logger.info("NO GPU AVAILABLE!")
 #endregion
 
-#region functions 
+#region functions
 def remove_comments(string):
     pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
     regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
@@ -60,7 +66,7 @@ def oneHotEncodeLabel(label):
 
 def transformData(example):
    data= {}
-   data["source_code"] = deleteNewlineAndGetters(remove_comments(example["source_code"])) 
+   data["source_code"] = deleteNewlineAndGetters(remove_comments(example["source_code"]))
    data["bytecodeOrigin"] = example["bytecode"]
    data["label"] = oneHotEncodeLabel(example["slither"])
    return data
@@ -96,17 +102,16 @@ class CustomDataset(Dataset):
             None,
             add_special_tokens=True,
             max_length=self.max_len,
-            pad_to_max_length=True,
-            return_token_type_ids=True
+            padding='max_length',
+            truncation=True,
+            return_token_type_ids=False
         )
         ids = inputs['input_ids']
         mask = inputs['attention_mask']
-        token_type_ids = inputs["token_type_ids"]
 
         return {
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
-            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
             'targets': torch.tensor(self.targets[index], dtype=torch.float)
         }
 
@@ -121,17 +126,15 @@ class CodeBERTAggregatedClass(torch.nn.Module):
         self.fc = torch.nn.Linear(self.codebert.config.hidden_size, num_classes)
         self.aggregation = aggregation
 
-    def forward(self, input_ids, attention_masks, token_type_ids):
+    def forward(self, input_ids, attention_masks):
         batch_size, seq_len = input_ids.size()
 
         # Divide input_ids e attention_masks in blocchi di 512 token
         num_chunks = (seq_len + 511) // 512
         input_ids = input_ids[:, :512*num_chunks].view(batch_size * num_chunks, 512)
         attention_masks = attention_masks[:, :512*num_chunks].view(batch_size * num_chunks, 512)
-        token_type_ids = token_type_ids[:, :512*num_chunks].view(batch_size * num_chunks, 512)
 
-        with torch.no_grad():
-            outputs = self.codebert(input_ids=input_ids, attention_mask=attention_masks, token_type_ids=token_type_ids)
+        outputs = self.codebert(input_ids=input_ids, attention_mask=attention_masks)
 
         last_hidden_states = outputs.last_hidden_state
         cls_tokens = last_hidden_states[:, 0, :]
@@ -153,28 +156,27 @@ class CodeBERTAggregatedClass(torch.nn.Module):
 #region train
 def train(epoch):
     model.train()
-    for batch_idx, data in enumerate(training_loader, 0):
-        print(f"Epoch: {epoch}")
-        print(f"Batch: {batch_idx}")
-        #print size of data 
-        print(data['ids'].size())
-        print(data['mask'].size())
-        print(data['token_type_ids'].size())
-
+    total_loss = 0
+    progress_bar = tqdm(enumerate(training_loader), total=len(training_loader))
+    for batch_idx, data in progress_bar:
         ids = data['ids'].to(DEVICE, dtype=torch.long)
         mask = data['mask'].to(DEVICE, dtype=torch.long)
-        token_type_ids = data['token_type_ids'].to(DEVICE, dtype=torch.long)
         targets = data['targets'].to(DEVICE, dtype=torch.float)
 
-        outputs = model(input_ids=ids, attention_masks=mask, token_type_ids=token_type_ids)
+        outputs = model(input_ids=ids, attention_masks=mask)
         loss = loss_fn(outputs, targets)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        total_loss += loss.item()
         if batch_idx % 10 == 0:
-            print(f'Epoch: {epoch}, Loss:  {loss.item()}')
+            progress_bar.set_description(f'Epoch {epoch}, Loss: {loss.item()}')
+
+    avg_loss = total_loss / len(training_loader)
+    logger.info(f'Epoch {epoch}, Average Loss: {avg_loss}')
+    torch.save(model.state_dict(), f'./stateDictMean/epoch_{epoch}_{MODEL}.pth')
 #endregion
 
 #region validate
@@ -183,16 +185,16 @@ def validate(epoch):
     fin_targets = []
     fin_outputs = []
     with torch.no_grad():
-        for _, data in enumerate(validation_loader, 0):
+        for _, data in tqdm(enumerate(validation_loader), total=len(validation_loader)):
             ids = data['ids'].to(DEVICE, dtype=torch.long)
             mask = data['mask'].to(DEVICE, dtype=torch.long)
-            token_type_ids = data['token_type_ids'].to(DEVICE, dtype=torch.long)
             targets = data['targets'].to(DEVICE, dtype=torch.float)
 
-            outputs = model(input_ids=ids, attention_masks=mask, token_type_ids=token_type_ids)
+            outputs = model(input_ids=ids, attention_masks=mask)
 
             fin_targets.extend(targets.cpu().detach().numpy().tolist())
             fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+
     return fin_outputs, fin_targets
 #endregion
 
@@ -202,23 +204,23 @@ training_set = CustomDataset(train_set, TOKENIZER)
 validation_set = CustomDataset(val_set, TOKENIZER)
 testing_set = CustomDataset(test_set, TOKENIZER)
 
-training_loader = DataLoader(training_set, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
-validation_loader = DataLoader(validation_set, batch_size=VALID_BATCH_SIZE, shuffle=False)
-testing_loader = DataLoader(testing_set, batch_size=VALID_BATCH_SIZE, shuffle=False)
+training_loader = DataLoader(training_set, batch_size=TRAIN_BATCH_SIZE, shuffle=True, pin_memory=True, num_workers=4)
+validation_loader = DataLoader(validation_set, batch_size=VALID_BATCH_SIZE, shuffle=False, pin_memory=True, num_workers=4)
+testing_loader = DataLoader(testing_set, batch_size=VALID_BATCH_SIZE, shuffle=False, pin_memory=True, num_workers=4)
 
 model = CodeBERTAggregatedClass(num_classes=NUM_CLASSES).to(DEVICE)
 optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
 loss_fn = nn.BCEWithLogitsLoss()
 
+best_accuracy = 0
+
 for epoch in range(EPOCHS):
-    print(f"Starting Epoch: {epoch}")
-    train(epoch) 
-    outputs, targets = validate(epoch)
-    outputs = np.array(outputs) >= 0.5
-    accuracy = metrics.accuracy_score(targets, outputs)
-    f1_score_micro = metrics.f1_score(targets, outputs, average='micro')
-    f1_score_macro = metrics.f1_score(targets, outputs, average='macro')
-    print(f"Accuracy Score = {accuracy}")
-    print(f"F1 Score (Micro) = {f1_score_micro}")
-    print(f"F1 Score (Macro) = {f1_score_macro}")
+    train(epoch)
+    fin_outputs, fin_targets = validate(epoch)
+    accuracy = metrics.accuracy_score(np.array(fin_targets), np.array(fin_outputs) > 0.5)
+    logger.info(f"Epoch {epoch}, Accuracy: {accuracy}")
+
+    if accuracy > best_accuracy:
+        torch.save(model.state_dict(), f'./stateDictMean/best_model_{MODEL}.pth')
+        best_accuracy = accuracy
 #endregion
