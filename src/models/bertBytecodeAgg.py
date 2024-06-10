eb18c903319ebb053bcde0dc6dd33c91a4b1f25f
MarcoTomasone <tomasone35@gmail.com>
2024-06-10 19:02:39 +0200

    add: bert agg

--- src/models/bertBytecodeAgg.py
+++ src/models/bertBytecodeAgg.py
@@ -3,8 +3,9 @@ import pandas as pd
 import re
 import torch
 import transformers
-from transformers import PreTrainedTokenizer, BertTokenizer, BertModel
+from transformers import BertTokenizer, BertModel
 from datasets import load_dataset
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torch import cuda
 from tqdm import tqdm
@@ -13,6 +14,7 @@ from sklearn import metrics
 
 # Constants
 MAX_LEN = 512
+CODE_BLOCKS = 2  # Number of 512-token blocks
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 16
 EPOCHS = 10
@@ -20,7 +22,6 @@ LEARNING_RATE = 1e-05
 NUM_CLASSES = 5
 SAFE_IDX = 4
 MODEL = 'bertBytecode'
-TOKENIZER_TYPE = 'hex'  # Options: 'hex', 'bert'
 DEVICE = 'cuda' if cuda.is_available() else 'cpu'
 
 if DEVICE == 'cuda':
@@ -33,49 +34,6 @@ if DEVICE == 'cuda':
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# HexTokenizer Class
-class HexTokenizer(PreTrainedTokenizer):
-    def __init__(self, max_length=None):
-        self.hex_dict = self._getHexDict()
-        self.ids_to_tokens = {i: self.hex_dict[i] for i in range(len(self.hex_dict))}
-        self.tokens_to_ids = {v: k for k, v in self.ids_to_tokens.items()}
-        self.max_length = max_length
-        super().__init__(vocab_size=len(self.hex_dict))
-
-    def _getHexDict(self):
-        hex_dict = {i: f"{j:02x}".upper() for i, j in enumerate(range(256))}
-        hex_dict[256] = '<UNK>'
-        hex_dict[257] = '<PAD>'
-        hex_dict[258] = '<INIT>'
-        hex_dict[259] = '<END>'
-        return hex_dict
-
-    def get_vocab(self):
-        return self.tokens_to_ids
-
-    def encode(self, text):
-        if self.max_length is not None and len(text) > self.max_length:
-            text = text[:self.max_length]
-
-        ids = []
-        for i in range(0, len(text), 2):
-            code = text[i:i+2]
-            if code == '0X':
-                ids.append(self.tokens_to_ids["<INIT>"])
-            elif code in self.tokens_to_ids:
-                ids.append(self.tokens_to_ids[code])
-            else:
-                ids.append(self.tokens_to_ids["<UNK>"])
-
-        for i in range(self.max_length // 2 - len(ids)):
-            ids.append(self.tokens_to_ids["<PAD>"])
-
-        ids[-1] = self.tokens_to_ids["<END>"]
-        return ids
-
-    def decode(self, ids):
-        return ''.join([self.ids_to_tokens[i] for i in ids])
-
 # Helper Functions
 def oneHotEncodeLabel(label):
     one_hot = np.zeros(NUM_CLASSES)
@@ -87,8 +45,13 @@ def oneHotEncodeLabel(label):
     return one_hot
 
 def transformData(example):
+    bytecode = example["bytecode"]
+    if bytecode.startswith("0x"):
+        bytecode = bytecode[2:]
+    bytecode_tokens = [bytecode[i:i+2] for i in range(0, len(bytecode), 2)]
+    bytecode_text = ' '.join(bytecode_tokens)  # Create a string with spaces between tokens
     return {
-        "bytecode": example["bytecode"],
+        "bytecode": bytecode_text,
         "label": oneHotEncodeLabel(example["slither"])
     }
 
@@ -100,43 +63,31 @@ def readAndPreprocessDataset():
 
 # Custom Dataset Class
 class CustomDataset(Dataset):
-    def __init__(self, dataframe, tokenizer, max_len, tokenizer_type):
+    def __init__(self, dataframe, tokenizer, max_len=MAX_LEN * CODE_BLOCKS):
         self.tokenizer = tokenizer
         self.data = dataframe
+        self.max_len = max_len
         self.bytecode = dataframe["bytecode"]
         self.targets = dataframe["label"]
-        self.max_len = max_len
-        self.tokenizer_type = tokenizer_type
 
     def __len__(self):
         return len(self.bytecode)
 
     def __getitem__(self, index):
         bytecode = str(self.bytecode[index])
-        # Remove the "0x" prefix if it exists
-        if bytecode.startswith("0x"):
-            bytecode = bytecode[2:]
 
-        # Split the bytecode into tokens of 2 characters each
-        bytecode_tokens = [bytecode[i:i+2] for i in range(0, len(bytecode), 2)]
+        if len(bytecode) == 0:
+            raise ValueError(f"Bytecode at index {index} is empty.")
 
-
-        if self.tokenizer_type == 'hex':
-            ids = self.tokenizer.encode(''.join(bytecode_tokens))
-            padding_length = self.max_len - len(ids)
-            ids += [self.tokenizer.tokens_to_ids["<PAD>"]] * padding_length
-            mask = [1] * len(ids)
-        else:
-            bytecode_text = ' '.join(bytecode_tokens)  # Create a string with spaces between tokens
-            encoding = self.tokenizer.encode_plus(
-                bytecode_text,
-                max_length=self.max_len,
-                padding='max_length',
-                truncation=True,
-                return_tensors='pt'
-            )
-            ids = encoding['input_ids'].flatten()
-            mask = encoding['attention_mask'].flatten()
+        encoding = self.tokenizer.encode_plus(
+            bytecode,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        ids = encoding['input_ids'].flatten()
+        mask = encoding['attention_mask'].flatten()
 
         return {
             'ids': torch.tensor(ids, dtype=torch.long),
@@ -144,21 +95,43 @@ class CustomDataset(Dataset):
             'targets': torch.tensor(self.targets[index], dtype=torch.float)
         }
 
-
 # BERT Model Class
-class BERTClass(torch.nn.Module):
-    def __init__(self):
-        super(BERTClass, self).__init__()
-        self.l1 = BertModel.from_pretrained('bert-base-uncased', cache_dir="./cache")
-        self.l2 = torch.nn.Dropout(0.3)
-        self.l3 = torch.nn.Linear(768, NUM_CLASSES)
-    
-    def forward(self, input_ids, attention_masks):
-        _, output_1 = self.l1(input_ids=input_ids, attention_mask=attention_masks, return_dict=False)
-        output_2 = self.l2(output_1)
-        return self.l3(output_2)
+class BERTAggregatedClass(torch.nn.Module):
+    def __init__(self, num_classes, aggregation='mean', dropout=0.3):
+        super(BERTAggregatedClass, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased', cache_dir="./cache")
+        self.dropout = torch.nn.Dropout(dropout)
+        self.fc = torch.nn.Linear(self.bert.config.hidden_size, num_classes)
+        self.aggregation = aggregation
 
-model = BERTClass()
+    def forward(self, input_ids, attention_masks):
+        batch_size, seq_len = input_ids.size()
+
+        # Divide input_ids e attention_masks in blocchi di 512 token
+        num_chunks = (seq_len + 511) // 512
+        input_ids = input_ids[:, :512*num_chunks].view(batch_size * num_chunks, 512)
+        attention_masks = attention_masks[:, :512*num_chunks].view(batch_size * num_chunks, 512)
+
+        # Ottieni l'output del modello BERT
+        _, last_hidden_states = self.bert(input_ids=input_ids, attention_mask=attention_masks, return_dict=False)
+
+        cls_tokens = last_hidden_states[:, 0, :]
+
+        cls_tokens = cls_tokens.view(batch_size, num_chunks, -1)
+
+        if self.aggregation == 'mean':
+            aggregated_output = torch.mean(cls_tokens, dim=1)
+        elif self.aggregation == 'max':
+            aggregated_output, _ = torch.max(cls_tokens, dim=1)
+        else:
+            raise ValueError("Aggregation must be 'mean' or 'max'")
+
+        aggregated_output = self.dropout(aggregated_output)
+        output = self.fc(aggregated_output)
+        return output
+
+
+model = BERTAggregatedClass(num_classes=NUM_CLASSES)
 model.to(DEVICE)
 
 def loss_fn(outputs, targets):
@@ -168,10 +141,10 @@ optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
 
 # Data Loading
 trainSet, testSet, valSet = readAndPreprocessDataset()
-tokenizer = HexTokenizer(max_length=MAX_LEN * 2) if TOKENIZER_TYPE == 'hex' else BertTokenizer.from_pretrained('bert-base-uncased')
-training_set = CustomDataset(trainSet, tokenizer, MAX_LEN, TOKENIZER_TYPE)
-testing_set = CustomDataset(testSet, tokenizer, MAX_LEN, TOKENIZER_TYPE)
-validation_set = CustomDataset(valSet, tokenizer, MAX_LEN, TOKENIZER_TYPE)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+training_set = CustomDataset(trainSet, tokenizer, MAX_LEN * CODE_BLOCKS)
+testing_set = CustomDataset(testSet, tokenizer, MAX_LEN * CODE_BLOCKS)
+validation_set = CustomDataset(valSet, tokenizer, MAX_LEN * CODE_BLOCKS)
 
 train_params = {'batch_size': TRAIN_BATCH_SIZE, 'shuffle': True, 'num_workers': 0}
 test_params = {'batch_size': VALID_BATCH_SIZE, 'shuffle': True, 'num_workers': 0}
@@ -179,15 +152,17 @@ test_params = {'batch_size': VALID_BATCH_SIZE, 'shuffle': True, 'num_workers': 0
 training_loader = DataLoader(training_set, **train_params)
 testing_loader = DataLoader(testing_set, **test_params)
 validation_loader = DataLoader(validation_set, **test_params)
+
 # Training Functions
+batch_losses = []
 epoch_losses = []
 epoch_accuracies = []
-
 def train(epoch):
     model.train()
     total_loss = 0.0
+    batch_losses = []
     progress_bar = tqdm(enumerate(training_loader), total=len(training_loader))
-    
+
     for batch_idx, data in progress_bar:
         ids = data['ids'].to(DEVICE, dtype=torch.long)
         mask = data['mask'].to(DEVICE, dtype=torch.long)
@@ -201,14 +176,17 @@ def train(epoch):
         optimizer.step()
 
         total_loss += loss.item()
+        batch_losses.append(loss.item())  # Add this line to track batch loss
+
         if batch_idx % 10 == 0:
             progress_bar.set_description(f'Epoch {epoch}, Loss: {loss.item()}')
 
     avg_loss = total_loss / len(training_loader)
     logger.info(f'Epoch {epoch}, Average Loss: {avg_loss}')
-    torch.save(model.state_dict(), f'./stateDict_{TOKENIZER_TYPE}/epoch_{epoch}_{MODEL}.pth')
+    torch.save(model.state_dict(), f'./stateDict_{MODEL}/epoch_{epoch}_{MODEL}.pth')
 
-    return avg_loss
+    return avg_loss, batch_losses  # Return batch losses
+
 
 def validate():
     model.eval()
@@ -229,35 +207,53 @@ def validate():
             fin_targets.extend(targets.cpu().detach().numpy().tolist())
             fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
 
-    avg_loss = total_loss / len(testing_loader)
+    avg_loss = total_loss / len(validation_loader)
     return avg_loss, fin_outputs, fin_targets
 
 def save_metrics_to_csv(epoch_losses, epoch_accuracies, filename='train-metrics.csv'):
     df = pd.DataFrame({'Epoch': list(range(1, EPOCHS + 1)), 'Loss': epoch_losses, 'Accuracy': epoch_accuracies})
     df.to_csv(filename, index=False)
 
+def plot_and_save_loss(epoch_losses, filename='loss_plot.png'):
+    plt.figure()
+    plt.plot(range(1, EPOCHS + 1), epoch_losses, marker='o', label='Training Loss')
+    plt.title('Training Loss over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
 best_val_loss = float('inf')
 print("Starting training")
 
+# Create the directory to save the state dicts and plots
+import os
+os.makedirs(f'./stateDict_{MODEL}', exist_ok=True)
+
+all_batch_losses = []  # Add this list to store all batch losses
+
 for epoch in range(EPOCHS):
     print(f"Starting Epoch: {epoch}")
-    avg_train_loss = train(epoch)
+    avg_train_loss, batch_losses = train(epoch)
     avg_val_loss, outputs, targets = validate()
 
-    outputs = np.array(outputs) >= 0.5
-    accuracy = metrics.accuracy_score(targets, outputs)
-    f1_score_micro = metrics.f1_score(targets, outputs, average='micro')
-    f1_score_macro = metrics.f1_score(targets, outputs, average='macro')
-    print(f"Validation Loss = {avg_val_loss}")
-    print(f"Accuracy Score = {accuracy}")
-    print(f"F1 Score (Micro) = {f1_score_micro}")
-    print(f"F1 Score (Macro) = {f1_score_macro}")
-
     epoch_losses.append(avg_train_loss)
-    epoch_accuracies.append(accuracy)
+    all_batch_losses.extend(batch_losses)  # Collect batch losses
+    val_accuracy = metrics.accuracy_score(np.array(targets), np.array(outputs) > 0.5)
+    epoch_accuracies.append(val_accuracy)
 
     if avg_val_loss < best_val_loss:
+        torch.save(model.state_dict(), f'./stateDict_{MODEL}/best_model_{MODEL}.pth')
         best_val_loss = avg_val_loss
-        torch.save(model.state_dict(), f'./stateDict_{TOKENIZER_TYPE}/best_model_{MODEL}.pth')
+
+    logger.info(f"Epoch {epoch}, Validation Loss: {avg_val_loss}, Validation Accuracy: {val_accuracy}")
 
 save_metrics_to_csv(epoch_losses, epoch_accuracies)
+plot_and_save_loss(all_batch_losses)
+
+def save_batch_losses_to_csv(batch_losses, filename='batch-losses.csv'):
+    df = pd.DataFrame({'Batch': list(range(1, len(batch_losses) + 1)), 'Loss': batch_losses})
+    df.to_csv(filename, index=False)
+
+save_batch_losses_to_csv(all_batch_losses)
